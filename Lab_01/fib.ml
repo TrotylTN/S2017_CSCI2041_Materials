faecafa9c14f79bfa3598cbb624481aedbfe2fef
zhou0745 <zhou0745@umn.edu>
2017-01-17 10:23:27 -0600

    Adding my name to the file

---
+++ Lab_01/fib.ml
@@ -0,0 +1,9 @@
+(* Author: Eric Van Wyk
+   Modified by: Tiannan Zhou *)
+
+(* A function computing the Fibonacci sequence: 1, 1, 2, 3, 5, 8, ... *)
+
+(* There is a bug in the following program.  Can you fix it? *)
+
+let rec fib x =
+  if x < 3 then 1 else fib (n-1) + fib (n-1)
