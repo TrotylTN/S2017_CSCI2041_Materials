3590640f8813c92bc44974e0bd50788693d67b3c
zhou0745 <zhou0745@umn.edu>
2017-01-24 17:19:04 -0600

    completed further questionns

--- Hwk_01/hwk_01.ml
+++ Hwk_01/hwk_01.ml
@@ -24,3 +24,55 @@ let frac_simplify (x,y) =
     else
       let gcdnum = euclid x y in
         (x / gcdnum, y / gcdnum)
+
+let square_approx n accuracy =
+  let rec approchfun lowb upb accu =
+    if upb -. lowb <= accu
+      then (lowb, upb)
+      else
+        let midnum = (lowb +. upb) /. 2. in
+          if midnum *. midnum > n
+            then approchfun lowb midnum accu
+            else approchfun midnum upb accu
+  in
+    approchfun 1.0 n accuracy
+
+let rec max_list alist =
+  match alist with
+  | [] -> 0
+  | hd::[] -> hd
+  | hd::tl ->
+    let rest_max = max_list tl in
+      if hd > rest_max
+        then hd
+        else rest_max
+
+let rec drop num alist =
+  match (num, alist) with
+  | (0, alist) -> alist
+  | (num, []) -> []
+  | (num, hd::tl) -> drop (num - 1) tl
+
+let rev alist =
+  let rec revfun thelist anslist =
+    match thelist with
+    | [] -> anslist
+    | hd::tl -> revfun tl (hd::anslist)
+  in
+    revfun alist []
+
+let distance (x1,y1) (x2,y2) =
+  let x_dis = x2 -. x1 in
+    let y_dis = y2 -. y1 in
+      sqrt (x_dis *. x_dis +. y_dis *. y_dis)
+
+let perimeter alist =
+  let rec calperimeter thelist firstarg =
+    match thelist with
+    | [] -> 0.
+    | hd::[] -> distance hd firstarg
+    | x1::(x2::tl) -> distance x1 x2 +. calperimeter (x2::tl) firstarg
+  in
+    match alist with
+    | [] -> 0.
+    | hd::tl -> calperimeter alist hd
