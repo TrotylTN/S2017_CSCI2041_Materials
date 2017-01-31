de035088d7bf5c14592008d71613ed6a44c18460
zhou0745 <zhou0745@umn.edu>
2017-01-31 15:16:55 -0600

    completed lab 3

--- Lab_03/hwk_01.ml
+++ Lab_03/hwk_01.ml
@@ -27,30 +27,42 @@ let frac_mul (x,y) (a,b) =
       raise (Failure "Incorrect input: denominator is zero")
 
 let frac_add (x,y) (a,b) =
-  (x * b + y * a, y * b)
+  if y != 0 && b != 0
+    then
+      (x * b + y * a, y * b)
+    else
+      raise (Failure "Incorrect input: denominator is zero")
 
 let frac_simplify (x,y) =
-  if x = 0
-    then (0,1)
+  if y = 0
+    then
+      raise (Failure "Incorrect input: denominator is zero")
     else
-      let gcdnum = euclid x y in
-        (x / gcdnum, y / gcdnum)
+      if x = 0
+        then (0,1)
+        else
+          let gcdnum = euclid x y in
+            (x / gcdnum, y / gcdnum)
 
 let square_approx n accuracy =
-  let rec approchfun lowb upb accu =
-    if upb -. lowb <= accu
-      then (lowb, upb)
-      else
-        let midnum = (lowb +. upb) /. 2. in
-          if midnum *. midnum > n
-            then approchfun lowb midnum accu
-            else approchfun midnum upb accu
-  in
-    approchfun 1.0 n accuracy
+  if n >= 1.
+  then
+    let rec approchfun lowb upb accu =
+      if upb -. lowb <= accu
+        then (lowb, upb)
+        else
+          let midnum = (lowb +. upb) /. 2. in
+            if midnum *. midnum > n
+              then approchfun lowb midnum accu
+              else approchfun midnum upb accu
+    in
+      approchfun 1.0 n accuracy
+  else
+    raise (Failure "Incorrect input: input must be not less than 1.0")
 
 let rec max_list alist =
   match alist with
-  | [] -> raise (Failure "Your list was empty")
+  | [] -> raise (Failure "Incorrect input: your list was empty")
   | hd::[] -> hd
   | hd::tl ->
     let rest_max = max_list tl in
@@ -91,7 +103,8 @@ let rec length l =
 	| [] -> 0
 	| e :: others -> 1 + length others
 
-(* Function is_matrix was implemented by Henry Middleton *)
+(* Function is_matrix was implemented by Henry Middleton*)
+(* Changes made by Tiannan Zhou *)
 (* Modified the base case since the original one does not work for matrix with
    just one line, such as [[1;2;3]] *)
 let rec is_matrix thing =
@@ -109,52 +122,65 @@ let rec list_scalar_add l n =
 
 (* Function matrix_scalar_add was implemented by Henry Middleton *)
 let rec matrix_scalar_add matrix num =
-	match matrix with
-	|[[]] -> [[]]
-	|x :: rest -> (list_scalar_add x num) :: (matrix_scalar_add rest num)
-	|[] -> []
+  if is_matrix matrix
+    then
+    	match matrix with
+    	|[[]] -> [[]]
+    	|x :: rest -> (list_scalar_add x num) :: (matrix_scalar_add rest num)
+    	|[] -> []
+    else
+      raise (Failure "Incorrect input: input not a matrix")
 
 let rec matrix_transpose amatrix =
-  let frontelement rowele =
-    match rowele with
-    | [] -> raise (Failure "frontelement")
-    | hd::tl -> hd
-  in
-  let backelement rowele =
-    match rowele with
-    | [] -> []
-    | hd::tl -> tl
-  in
-  let rec gethead thematrix =
-    match thematrix with
-    | [] -> []
-    | hd::tl -> (frontelement hd)::(gethead tl)
-  in
-  let rec getback thematrix =
-    match thematrix with
-    | [] -> []
-    | hd::tl -> (backelement hd)::(getback tl)
-  in
-    match amatrix with
-    | [] -> []
-    | []::tl -> []
-    | hd::tl -> (gethead amatrix)::(matrix_transpose (getback amatrix))
-
-let rec matrix_multiply amatrix bmatrix =
-  let transmatrix = matrix_transpose bmatrix in
-    let rec timeselement alist blist =
-      match (alist, blist) with
-      | ([],[]) -> 0
-      | (_::_, []) -> raise (Failure "invalid matrix")
-      | ([], _::_) -> raise (Failure "invalid matrix")
-      | (ahd::atl,bhd::btl) ->
-        ahd * bhd + timeselement atl btl
-    in
-      let rec gothrubmatrix alist bmatrix =
-        match bmatrix with
+  if is_matrix amatrix
+    then
+      let frontelement rowele =
+        match rowele with
+        | [] -> raise (Failure "frontelement: empty list")
+        | hd::tl -> hd
+      in
+      let backelement rowele =
+        match rowele with
+        | [] -> raise (Failure "backelement: empty list")
+        | hd::tl -> tl
+      in
+      let rec gethead thematrix =
+        match thematrix with
         | [] -> []
-        | hd::tl -> (timeselement alist hd)::gothrubmatrix alist tl
+        | hd::tl -> (frontelement hd)::(gethead tl)
+      in
+      let rec getback thematrix =
+        match thematrix with
+        | [] -> []
+        | hd::tl -> (backelement hd)::(getback tl)
       in
         match amatrix with
         | [] -> []
-        | hd::tl -> (gothrubmatrix hd transmatrix)::(matrix_multiply tl bmatrix)
+        | []::tl -> []
+        | hd::tl -> (gethead amatrix)::(matrix_transpose (getback amatrix))
+    else
+      raise (Failure "Incorrect input: input not a matrix")
+
+
+let rec matrix_multiply amatrix bmatrix =
+  if is_matrix amatrix && is_matrix bmatrix
+    then
+      let transmatrix = matrix_transpose bmatrix in
+        let rec timeselement alist blist =
+          match (alist, blist) with
+          | ([],[]) -> 0
+          | (_::_, []) -> raise (Failure "invalid matrix")
+          | ([], _::_) -> raise (Failure "invalid matrix")
+          | (ahd::atl,bhd::btl) ->
+            ahd * bhd + timeselement atl btl
+        in
+          let rec gothrubmatrix alist bmatrix =
+            match bmatrix with
+            | [] -> []
+            | hd::tl -> (timeselement alist hd)::gothrubmatrix alist tl
+          in
+            match amatrix with
+            | [] -> []
+            | hd::tl -> (gothrubmatrix hd transmatrix)::(matrix_multiply tl bmatrix)
+    else
+      raise (Failure "Incorrect input: input not a matrix")
