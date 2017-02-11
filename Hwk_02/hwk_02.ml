3dd1196c8db01e924ea8bb1d296f91277b63ef7d
zhou0745 <zhou0745@umn.edu>
2017-02-11 17:01:59 -0600

    Completed the first a few requirments

--- Hwk_02/hwk_02.ml
+++ Hwk_02/hwk_02.ml
@@ -5,29 +5,43 @@
    'is_elem_by', 'is_elem', 'dedup', and 'split_by' here. *)
 
 let rec take n l = match l with
-  | [] -> [] 
+  | [] -> []
   | x::xs -> if n > 0 then x::take (n-1) xs else []
 
 let rec drop n l = match l with
-  | [] -> [] 
+  | [] -> []
   | x::xs -> if n > 0 then drop (n-1) xs else l
 
-let rev lst = raise (Failure "This function is not yet implemented!")
+let length alist = List.length alist
 
+let rev alist = List.rev alist
 
+let is_elem_by f n alist =
+  List.fold_left (fun p_ans t_num -> p_ans ||(f t_num n)) false alist
+
+let is_elem n alist = is_elem_by (=) n alist
+
+let dedup alist =
+  let templist = List.sort (fun a b -> if a < b then -1 else 1) alist
+  in
+    let checksame cur_num pre_list =
+      match pre_list with
+      | hd :: restlist -> if hd = cur_num then pre_list else cur_num :: pre_list
+      | [] -> [cur_num]
+    in List.fold_right checksame templist []
 
 (* Some functions for reading files. *)
 let read_file (filename:string) : char list option =
   let rec read_chars channel sofar =
-    try 
+    try
       let ch = input_char channel
       in read_chars channel (ch :: sofar)
     with
     | _ -> sofar
   in
-  try 
+  try
     let channel = open_in filename
-    in 
+    in
     let chars_in_reverse = read_chars channel []
     in Some (rev chars_in_reverse)
   with
@@ -36,8 +50,8 @@ let read_file (filename:string) : char list option =
 
 
 
-type result = OK 
+type result = OK
 	    | FileNotFound of string
-	    | IncorrectNumLines of int 
+	    | IncorrectNumLines of int
 	    | IncorrectLines of (int * int) list
 	    | IncorrectLastStanza
