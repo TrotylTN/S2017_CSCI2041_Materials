e65cfbd3ec53cba284af2a79ddbbfbd1858c793c
zhou0745 <zhou0745@umn.edu>
2017-03-11 19:49:01 -0600

    Completed show_expr

--- Hwk_04/arithmetic.ml
+++ Hwk_04/arithmetic.ml
@@ -1,7 +1,16 @@
 
-type expr 
+type expr
   = Const of int
   | Add of  expr * expr
   | Mul of expr * expr
   | Sub of expr * expr
   | Div of expr * expr
+
+
+let rec show_expr (e: expr) : string =
+  match e with
+  | Const (n) -> string_of_int n
+  | Add (e1, e2) -> "(" ^ show_expr e1 ^ "+" ^ show_expr e2 ^ ")"
+  | Mul (e1, e2) -> "(" ^ show_expr e1 ^ "*" ^ show_expr e2 ^ ")"
+  | Sub (e1, e2) -> "(" ^ show_expr e1 ^ "-" ^ show_expr e2 ^ ")"
+  | Div (e1, e2) -> "(" ^ show_expr e1 ^ "/" ^ show_expr e2 ^ ")"
