0e4a75e1ea03ae3c38eab1c6204759dc53252ca2
zhou0745 <zhou0745@umn.edu>
2017-03-30 16:06:06 -0500

    Completed first 2 functions of part 3

--- Hwk_05/streams.ml
+++ Hwk_05/streams.ml
@@ -109,58 +109,20 @@ let primes = sieve (from 2)
 (* All code above is creitted to Eric Van Wyk *)
 
 (* All following code is wrote by Tiannan Zhou *)
-let rec str_from (n : int) : string stream =
-  Cons ( string_of_int n,
-         fun () -> print_endline ("step " ^ string_of_int (n+1)) ;
-                   str_from (n+1) )
-
-let str_nats = str_from 1
-
-let separators (n : int) (sep : string) : string stream =
-  let rec rep_sep (i : int) (cnt : int): string stream =
-    match i with
-    | 0 -> Cons ( "\n",
-                  fun () -> print_endline ("step " ^ string_of_int (cnt+1)) ;
-                            rep_sep n (cnt + 1))
-    | _ -> Cons ( sep,
-                  fun () -> print_endline ("step " ^ string_of_int (cnt+1)) ;
-                            rep_sep (i - 1) (cnt + 1))
-  in
-    rep_sep n 0
-
-let rec alternate s1 s2 =
-  match (s1, s2) with
-  | Cons (h1, t1), Cons (h2,t2) -> Cons (h1 , fun () -> Cons(h2, fun ()-> alternate (t1()) (t2 ())))
-(*
-
-let alternate (s1 : 'a stream) (s2 : 'a stream) :  'a stream =
-  let rec get_last_ele alist =
-    match alist with
-    | [x] -> x
-    | hd::tl -> get_last_ele tl
-    | [ ] -> raise (Failure "empty list")
-  in
-  let rec alternate_help (cnt : int) =
-    if cnt mod 2 = 0
-      then
-        Cons ( get_last_ele (take (cnt / 2 + 1) s1),
-                fun () -> print_endline ("step " ^ string_of_int (cnt+1)) ;
-                          alternate_help (cnt + 1))
-      else
-        Cons ( get_last_ele (take (cnt / 2 + 1) s2),
-                fun () -> print_endline ("step " ^ string_of_int (cnt+1)) ;
-                          alternate_help (cnt + 1))
-  in
-    alternate_help 0 *)
-
-let ten_per_line = separators 9 ", "
-
-let str_105_nats = List.fold_right (fun e rrest -> e ^ rrest)
-                                   (take 209 (alternate str_nats ten_per_line))
-                                   ""
-(* All code above is creitted to Eric Van Wyk *)
-
-(* All following code is wrote by Tiannan Zhou *)
 let rec cubes_from (n : int) : int stream =
   Cons ( n * n * n,
          fun () -> cubes_from (n+1) )
+
+let rec drop (n : int) (s: 'a stream) : 'a stream =
+  if n = 0
+    then
+      s
+    else
+      drop (n - 1) (tail s)
+
+let rec drop_until (f: 'a -> bool) (s: 'a stream) : 'a stream =
+  if (f (head s) )
+    then
+      s      
+    else
+      drop_until f (tail s)
