3290395b70e7d8881ce2dcae79e968b2f86da1fb
zhou0745 <zhou0745@umn.edu>
2017-04-16 22:41:52 -0500

    Completed Homework

--- Hwk_06/maze.ml
+++ Hwk_06/maze.ml
@@ -25,3 +25,45 @@ let maze_moves (x,y) =
   | (5,3) -> [(5,2); (4,3); (5,4)]
   | (5,4) -> [(5,3)]
   | (5,5) -> [(4,5)]
+
+let is_elem v l =
+  List.fold_right (fun x in_rest -> if x = v then true else in_rest) l false
+
+type loc = int * int
+
+let rec search_route (x,y) (past_route: loc list) : loc list option =
+  let loclist = maze_moves (x,y)
+  in
+  let
+    check_arrive (x,y) =
+      match (x,y) with
+      | (3,5) -> true
+      | (5,1) -> true
+      | _ -> false
+  in
+  let rec forloc loclist =
+    match loclist with
+    | pos::tl ->
+    (
+      match check_arrive pos with
+      | true -> Some (past_route @ [pos])
+      | false ->
+        if (is_elem pos past_route)
+          then forloc tl
+          else
+          (
+            match (search_route pos (past_route @ [pos])) with
+            | None -> forloc tl
+            | Some route -> Some route
+          )
+    )
+    | [ ] -> None
+  in
+    forloc loclist
+
+exception NotFound
+
+let maze (a: unit) : loc list =
+  match search_route (2,3) [(2,3)] with
+  | Some route -> route
+  | None -> raise NotFound
