ae643430ed68982110c500be435d5324646107c5
zhou0745 <zhou0745@umn.edu>
2017-04-24 03:11:45 -0500

    Completed int part of vectors

--- Hwk_07/hwk_07.ml
+++ Hwk_07/hwk_07.ml
@@ -45,22 +45,22 @@ module Make_vector (BaseType: Base_type ) :
     match v with
     | Vec(alist) -> Vec(List.map (BaseType.mul b) alist)
 
+  let size (v: vec) : int =
+  match v with
+  | Vec(alist) -> List.length alist
+
   let scalar_prod (v1: vec) (v2: vec) : base option =
     if size v1 != size v2
       then None
       else match (v1, v2) with
-      | Vec(l1, l2) -> Some(BaseType.dot l1 l2)
-
-  let size (v: vec) : int =
-    match v with
-    | Vec(alist) -> List.length alist
+      | Vec(l1), Vec(l2) -> Some(BaseType.dot l1 l2)
 
   let to_string (v: vec) : string =
     let help_fun a b =
       ", " ^ (BaseType.to_string a) ^ b
     in
       match v with
-      | Vec(hd,tl) ->
+      | Vec(hd::tl) ->
           "<< " ^
           string_of_int(size v) ^
           " | " ^
@@ -71,14 +71,53 @@ module Make_vector (BaseType: Base_type ) :
 end
 
 module Int_arithmetic : (Base_type with type t = int) = struct
+  type t = int
+
+  let add (a: int) (b: int) : int = a + b
+
+  let mul (a: int) (b: int) : int = a * b
+
+  let rec listlize (size: int) (v: int) : int list =
+    match size with
+    | 0 -> [ ]
+    | _ -> v :: (listlize (size - 1) v)
+
+  let to_string = string_of_int
+
+  let rec dot l1 l2 =
+    match l1, l2 with
+    | hd1::tl1, hd2::tl2 -> hd1 * hd2 + dot tl1 tl2
+    | _, _ -> 0
 
 end
 
 
-module Complex_arithmetic : (Base_type with type t = float * float) = struct
+(* module Complex_arithmetic : (Base_type with type t = float * float) = struct
 
-end
+end *)
 
 module Int_vector = Make_vector (Int_arithmetic)
 
-module Complex_vector = Make_vector (Complex_arithmetic)
+(* module Complex_vector = Make_vector (Complex_arithmetic) *)
+
+let v1 = Int_vector.create 10 1
+
+let v2 = Int_vector.from_list [1;2;3;4;5]
+
+let v3 = Int_vector.scalar_add 3 v2
+
+let v4 = Int_vector.scalar_mul 10 v2
+
+let i1 = Int_vector.scalar_prod v3 v4
+
+let l1 = Int_vector.to_list v3
+
+let i2 = Int_vector.size v4
+
+let s1 = Int_vector.to_string v1
+
+let s2 = Int_vector.to_string v2
+
+let s3 = Int_vector.to_string v3
+
+let s4 = Int_vector.to_string v4
